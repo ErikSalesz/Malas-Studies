e10f4b84b7b4f26a3c8a85af0503cdc197c62200
ErikS <erikcsales1234@gmail.com>
2025-09-12 15:17:04 -0300

    fix(ui): Restaura o posicionamento correto da linha de hora atual

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js (VERSÃO ATUALIZADA E MELHORADA)
 
 // 1. Atualizamos a versão do cache
-const CACHE_NAME = 'meu-pwa-cache-v15';
+const CACHE_NAME = 'meu-pwa-cache-v16';
 
 // 2. Atualizamos a lista de arquivos para refletir a nova estrutura
 const urlsToCache = [
